a2a69c08e40aaf7801a93b0a0615c4e7713551cc
gcdelgado <gcdelgado@gmail.com>
2015-08-19 02:26:19 -0300

    Desafio 1 - editais 12 e 13
    Resolvendo o bug do autor para proposições do Senado

--- src/main/java/br/gov/mj/sislegis/app/parser/senado/ParserProposicaoSenado.java
+++ src/main/java/br/gov/mj/sislegis/app/parser/senado/ParserProposicaoSenado.java
@@ -1,12 +1,12 @@
 package br.gov.mj.sislegis.app.parser.senado;
 
-import java.net.URL;
-import java.util.List;
-
 import br.gov.mj.sislegis.app.enumerated.Origem;
 import br.gov.mj.sislegis.app.model.Proposicao;
-
 import com.thoughtworks.xstream.XStream;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
 
 public class ParserProposicaoSenado {
 	
@@ -29,7 +29,7 @@ public class ParserProposicaoSenado {
 
 		xstream.fromXML(url, detalheMateria);
 		
-		Proposicao proposicao = new Proposicao();
+		Proposicao proposicao = new ProposicaoSenado();
 		
 		proposicao = detalheMateria.getProposicoes().isEmpty() ? proposicao : detalheMateria.getProposicoes().get(0);
 		proposicao.setOrigem(Origem.SENADO);
@@ -40,23 +40,55 @@ public class ParserProposicaoSenado {
 	
 	private static void config(XStream xstream) {
 		xstream.alias("DetalheMateria", DetalheMateria.class);
-		xstream.alias("Materia", Proposicao.class);
-		
+		xstream.alias("Materia", ProposicaoSenado.class);
+		xstream.alias("Autor", Autor.class);
+
 		xstream.aliasField("Materias", DetalheMateria.class, "proposicoes");
-		
-		xstream.aliasField("Codigo", Proposicao.class, "idProposicao");
-		xstream.aliasField("Subtipo", Proposicao.class, "tipo");
-		xstream.aliasField("Numero", Proposicao.class, "numero");
-		xstream.aliasField("Ano", Proposicao.class, "ano");
-		xstream.aliasField("Ementa", Proposicao.class, "ementa");
+		xstream.aliasField("Autoria", ProposicaoSenado.class, "autores");
+
+		xstream.aliasField("Codigo", ProposicaoSenado.class, "idProposicao");
+		xstream.aliasField("Subtipo", ProposicaoSenado.class, "tipo");
+		xstream.aliasField("Numero", ProposicaoSenado.class, "numero");
+		xstream.aliasField("Ano", ProposicaoSenado.class, "ano");
+		xstream.aliasField("Ementa", ProposicaoSenado.class, "ementa");
+		xstream.aliasField("Nome", Autor.class, "nome");
+
 	}
 }
 
 class DetalheMateria {
 
-	protected List<Proposicao> proposicoes;
+	protected List<ProposicaoSenado> proposicoes;
 
-	protected List<Proposicao> getProposicoes() {
+	protected List<ProposicaoSenado> getProposicoes() {
 		return proposicoes;
+	}
+}
+
+class ProposicaoSenado extends Proposicao {
+	protected List<Autor> autores;
+
+	protected List<Autor> getAutores() {
+		return autores;
+	}
+
+	@Override
+	public String getAutor() {
+		if (Objects.isNull(autores) || autores.size() == 0) {
+			return super.getAutor();
+
+		} else {
+			return autores.get(0).getNome();
+		}
+
+	}
+
+}
+
+class Autor {
+	protected String nome;
+
+	protected String getNome() {
+		return nome;
 	}
 }
