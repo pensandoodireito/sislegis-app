70e42f2016f69e00a14ba6448319f746e8495030
gcdelgado <gcdelgado@gmail.com>
2015-08-19 21:00:37 -0300

    Desafio 1 dos editais 12 e 13
    Convertendo a entidade Proposicao para um DTO que se adeque ao formato
    do XML retornado pelo serviço do Senado

--- src/main/java/br/gov/mj/sislegis/app/parser/senado/ParserProposicaoSenado.java
+++ src/main/java/br/gov/mj/sislegis/app/parser/senado/ParserProposicaoSenado.java
@@ -9,73 +9,86 @@ import java.util.List;
 import java.util.Objects;
 
 public class ParserProposicaoSenado {
-	
+
 	public static void main(String[] args) throws Exception {
 		ParserProposicaoSenado parser = new ParserProposicaoSenado();
 		Long idProposicao = 24257L; // TODO: Informação que vem do filtro
 		System.out.println(parser.getProposicao(idProposicao).toString());
 	}
-	
+
 	public Proposicao getProposicao(Long idProposicao) throws Exception {
-		String wsURL = "http://legis.senado.leg.br/dadosabertos/materia/"+idProposicao+"?v=3";
+		String wsURL = "http://legis.senado.leg.br/dadosabertos/materia/" + idProposicao + "?v=3";
 		URL url = new URL(wsURL);
-		
+
 		XStream xstream = new XStream();
 		xstream.ignoreUnknownElements();
-		
+
 		DetalheMateria detalheMateria = new DetalheMateria();
-		
+
 		config(xstream);
 
 		xstream.fromXML(url, detalheMateria);
-		
-		Proposicao proposicao = new ProposicaoSenado();
-		
-		proposicao = detalheMateria.getProposicoes().isEmpty() ? proposicao : detalheMateria.getProposicoes().get(0);
+
+		Proposicao proposicao = new Proposicao();
+
+		proposicao = detalheMateria.getProposicoes().isEmpty() ? proposicao : detalheMateria.getProposicoes().get(0).converterParaProposicao();
 		proposicao.setOrigem(Origem.SENADO);
-		proposicao.setLinkProposicao("http://www.senado.leg.br/atividade/materia/detalhes.asp?p_cod_mate="+proposicao.getIdProposicao());
-		
+		proposicao.setLinkProposicao("http://www.senado.leg.br/atividade/materia/detalhes.asp?p_cod_mate=" + proposicao.getIdProposicao());
+
 		return proposicao;
 	}
-	
+
 	private static void config(XStream xstream) {
 		xstream.alias("DetalheMateria", DetalheMateria.class);
-		xstream.alias("Materia", ProposicaoSenado.class);
+		xstream.alias("Materia", ProposicaoSenadoDTO.class);
 		xstream.alias("Autor", Autor.class);
 
 		xstream.aliasField("Materias", DetalheMateria.class, "proposicoes");
-		xstream.aliasField("Autoria", ProposicaoSenado.class, "autores");
+		xstream.aliasField("Autoria", ProposicaoSenadoDTO.class, "autores");
 
-		xstream.aliasField("Codigo", ProposicaoSenado.class, "idProposicao");
-		xstream.aliasField("Subtipo", ProposicaoSenado.class, "tipo");
-		xstream.aliasField("Numero", ProposicaoSenado.class, "numero");
-		xstream.aliasField("Ano", ProposicaoSenado.class, "ano");
-		xstream.aliasField("Ementa", ProposicaoSenado.class, "ementa");
+		xstream.aliasField("Codigo", ProposicaoSenadoDTO.class, "idProposicao");
+		xstream.aliasField("Subtipo", ProposicaoSenadoDTO.class, "tipo");
+		xstream.aliasField("Numero", ProposicaoSenadoDTO.class, "numero");
+		xstream.aliasField("Ano", ProposicaoSenadoDTO.class, "ano");
+		xstream.aliasField("Ementa", ProposicaoSenadoDTO.class, "ementa");
 		xstream.aliasField("Nome", Autor.class, "nome");
-
 	}
+
 }
 
 class DetalheMateria {
 
-	protected List<ProposicaoSenado> proposicoes;
+	protected List<ProposicaoSenadoDTO> proposicoes;
 
-	protected List<ProposicaoSenado> getProposicoes() {
+	protected List<ProposicaoSenadoDTO> getProposicoes() {
 		return proposicoes;
 	}
 }
 
-class ProposicaoSenado extends Proposicao {
+class ProposicaoSenadoDTO {
+	protected Integer idProposicao;
+	protected String tipo;
+	protected String numero;
+	protected String ano;
+	protected String ementa;
 	protected List<Autor> autores;
 
-	protected List<Autor> getAutores() {
-		return autores;
+	protected Proposicao converterParaProposicao() {
+		Proposicao proposicao = new Proposicao();
+		proposicao.setIdProposicao(idProposicao);
+		proposicao.setTipo(tipo);
+		proposicao.setNumero(numero);
+		proposicao.setAno(ano);
+		proposicao.setEmenta(ementa);
+
+		proposicao.setAutor(converterAutorUnico());
+
+		return proposicao;
 	}
 
-	@Override
-	public String getAutor() {
+	private String converterAutorUnico() {
 		if (Objects.isNull(autores) || autores.size() == 0) {
-			return super.getAutor();
+			return null;
 
 		} else {
 			return autores.get(0).getNome();
