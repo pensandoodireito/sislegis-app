03000284b7b7519e811a3c36105e52c92cf79529
Gustavo Delgado <gcdelgado@gmail.com>
2015-10-12 15:10:34 -0300

    Adicionando usuario logado ao andamento

--- src/main/java/br/gov/mj/sislegis/app/rest/AndamentoEndpoint.java
+++ src/main/java/br/gov/mj/sislegis/app/rest/AndamentoEndpoint.java
@@ -1,6 +1,8 @@
 package br.gov.mj.sislegis.app.rest;
 
 import br.gov.mj.sislegis.app.model.Andamento;
+import br.gov.mj.sislegis.app.model.Usuario;
+import br.gov.mj.sislegis.app.rest.authentication.UsuarioAutenticadoBean;
 import br.gov.mj.sislegis.app.service.AndamentoService;
 
 import javax.inject.Inject;
@@ -8,6 +10,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
 import java.util.List;
 
 @Path("/andamentos")
@@ -16,10 +19,22 @@ public class AndamentoEndpoint {
     @Inject
     private AndamentoService andamentoService;
 
+    @Inject
+    private UsuarioAutenticadoBean controleUsuarioAutenticado;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response save(Andamento andamento) {
-        andamentoService.save(andamento);
+    public Response save(Andamento andamento, @HeaderParam("Authorization") String authorization) {
+        try {
+            Usuario usuario = controleUsuarioAutenticado.carregaUsuarioAutenticado(authorization);
+            andamento.setUsuario(usuario);
+            andamentoService.save(andamento);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+
         return Response.created(UriBuilder.fromResource(AndamentoEndpoint.class).path(String.valueOf(andamento.getId())).build()).build();
     }
 
