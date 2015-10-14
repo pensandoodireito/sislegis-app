d35da753be7864681505a168331cd83ec8eb747a
Gustavo Delgado <gcdelgado@gmail.com>
2015-10-14 01:10:14 -0300

    idProposicao de Andamento adicionado em JsonIgnoreProperties

--- src/main/java/br/gov/mj/sislegis/app/model/Andamento.java
+++ src/main/java/br/gov/mj/sislegis/app/model/Andamento.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 @Entity
 @XmlRootElement
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties({"idProposicao"})
 public class Andamento implements AbstractEntity{
 
     @Id
