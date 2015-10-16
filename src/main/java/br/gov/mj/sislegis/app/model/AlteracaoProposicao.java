ef7ff0fd22c73f8b92759bf9cce20e9c37a40ab3
Rafael Coutinho <rafaelcoutinho@gmail.com>
2015-10-16 11:50:09 -0300

    Altera AlteracaoProposicao para extender de AbstractEntity

--- src/main/java/br/gov/mj/sislegis/app/model/AlteracaoProposicao.java
+++ src/main/java/br/gov/mj/sislegis/app/model/AlteracaoProposicao.java
@@ -24,7 +24,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Entity
 @XmlRootElement
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class AlteracaoProposicao implements AbstractEntity, Comparable<AlteracaoProposicao> {
+public class AlteracaoProposicao extends AbstractEntity implements Comparable<AlteracaoProposicao> {
 
 	private static final long serialVersionUID = 7949894944142814382L;
 
@@ -72,31 +72,6 @@ public class AlteracaoProposicao implements AbstractEntity, Comparable<Alteracao
 
 	public Date getData() {
 		return data;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Proposicao)) {
-			return false;
-		}
-		AlteracaoProposicao other = (AlteracaoProposicao) obj;
-		if (id != null) {
-			if (!id.equals(other.id)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
 	}
 
 	public void setProposicao(Proposicao proposicao) {
