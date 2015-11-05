4a8c3945fd160253462866d73a4b13d9cf700d91
Rafael Coutinho <rafaelcoutinho@gmail.com>
2015-11-05 12:58:34 -0200

    Coloca a sigla no serializer

--- src/main/java/br/gov/mj/sislegis/app/rest/serializers/CompactProposicaoSerializer.java
+++ src/main/java/br/gov/mj/sislegis/app/rest/serializers/CompactProposicaoSerializer.java
@@ -8,17 +8,18 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 
-
 public class CompactProposicaoSerializer extends JsonSerializer<Proposicao> {
 
-    @Override
-    public void serialize(Proposicao value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
-        jgen.writeStartObject();
-        jgen.writeNumberField("id", value.getId());
-        jgen.writeStringField("ementa", value.getEmenta());
-        jgen.writeStringField("tipo", value.getTipo());
-        jgen.writeStringField("numero", value.getNumero());
-        jgen.writeStringField("ano", value.getAno());
-        jgen.writeEndObject();
-    }
+	@Override
+	public void serialize(Proposicao value, JsonGenerator jgen, SerializerProvider provider) throws IOException,
+			JsonProcessingException {
+		jgen.writeStartObject();
+		jgen.writeNumberField("id", value.getId());
+		jgen.writeStringField("ementa", value.getEmenta());
+		jgen.writeStringField("tipo", value.getTipo());
+		jgen.writeStringField("numero", value.getNumero());
+		jgen.writeStringField("ano", value.getAno());
+		jgen.writeStringField("sigla", value.getSigla());
+		jgen.writeEndObject();
+	}
 }
