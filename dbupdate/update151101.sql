bcc13b41b6278fb007ac3da79129ac1151dc7df8
Gustavo Delgado <gcdelgado@gmail.com>
2015-11-11 21:28:55 -0200

    Passando script sql para o formato do liquibase pensandoodireito/sislegis-app#169

--- dbupdate/update151101.sql
+++ dbupdate/update151101.sql
@@ -1,1 +1,2 @@
 --liquibase formatted sql
+CREATE TABLE posicionamento_proposicao(id BIGINT PRIMARY KEY NOT NULL, datacriacao TIMESTAMP, posicionamento_id BIGINT, proposicao_id BIGINT, usuario_id BIGINT, FOREIGN KEY (posicionamento_id) REFERENCES posicionamento (id), FOREIGN KEY (proposicao_id) REFERENCES proposicao (id), FOREIGN KEY (usuario_id) REFERENCES usuario (id));
