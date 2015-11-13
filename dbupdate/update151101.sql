04f6f4a43d1bf1cfe1866c10d09856d562ee16b0
Rafael Coutinho <rafaelcoutinho@gmail.com>
2015-11-13 11:33:28 -0200

    Evita migrar tarefas finalizadas pensandoodireito/sislegis-app#251

--- dbupdate/update151101.sql
+++ dbupdate/update151101.sql
@@ -21,7 +21,7 @@ ALTER TABLE notificacao ADD CONSTRAINT fk_not_user FOREIGN KEY (usuario_id)
 --rollback ALTER TABLE notificacao drop CONSTRAINT fk_not_user
 
 --changeset release151101:151101-6
-insert into notificacao (id,categoria,usuario_id,criada_em,vista_em,descricao,identificacao_entidade) select nextval ('hibernate_sequence'),'TAREFAS',t.usuario_id,now(),NULL,ept.nome,t.id from tarefa t,encaminhamentoproposicao ep,tipo_encaminhamento ept where t.encaminhamentoproposicao_id=ep.id and ept.id=ep.tipo_encaminhamento_id
+insert into notificacao (id,categoria,usuario_id,criada_em,vista_em,descricao,identificacao_entidade) select nextval ('hibernate_sequence'),'TAREFAS',t.usuario_id,now(),NULL,ept.nome,t.id from tarefa t,encaminhamentoproposicao ep,tipo_encaminhamento ept where t.isfinalizada=true and  t.encaminhamentoproposicao_id=ep.id and ept.id=ep.tipo_encaminhamento_id
 --rollback delete from notificacao
 
 --changeset release151101:151101-7
