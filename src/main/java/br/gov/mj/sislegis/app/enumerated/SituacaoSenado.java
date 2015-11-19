8aa0ccfe05ae8b2d77e594f4738c73762d71698f
Rafael Coutinho <rafaelcoutinho@gmail.com>
2015-11-19 21:05:58 -0200

    Correção de remoção de item da pauta HOTFIX

---
+++ src/main/java/br/gov/mj/sislegis/app/enumerated/SituacaoSenado.java
@@ -0,0 +1,29 @@
+package br.gov.mj.sislegis.app.enumerated;
+
+import br.gov.mj.sislegis.app.model.pautacomissao.SituacaoSessao;
+
+/**
+ * Representa as situacoes das pautas de reuniao do Senado
+ */
+public enum SituacaoSenado {
+	Encerrada, Realizada, Agendada, Cancelada, Convocada, Adiada, Aberta, EmAndamento;
+
+	public SituacaoSessao situacaoSessaoCorrespondente() {
+		switch (this) {
+		case Encerrada:
+		case Realizada:
+			return SituacaoSessao.Realizada;
+		case Agendada:
+		case Adiada:
+		case Convocada:
+		case Aberta:
+		case EmAndamento:
+			return SituacaoSessao.Agendada;
+		case Cancelada:
+			return SituacaoSessao.Cancelada;
+		default:
+			return SituacaoSessao.Desconhecido;
+		}
+	}
+
+}
