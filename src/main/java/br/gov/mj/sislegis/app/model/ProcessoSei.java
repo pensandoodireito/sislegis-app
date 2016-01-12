afe00732425599a05a549af83a88d8391fcdb781
Gustavo Delgado <gcdelgado@gmail.com>
2016-01-12 19:59:15 -0200

    mapeamento jpa pensandoodireito/sislegis-app#316

--- src/main/java/br/gov/mj/sislegis/app/model/ProcessoSei.java
+++ src/main/java/br/gov/mj/sislegis/app/model/ProcessoSei.java
@@ -1,9 +1,39 @@
 package br.gov.mj.sislegis.app.model;
 
-public class ProcessoSei {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "processo_sei")
+public class ProcessoSei extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne(optional = false)
     private Proposicao proposicao;
+
     private String nup;
+
     private String linkSei;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Proposicao getProposicao() {
         return proposicao;
