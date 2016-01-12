c76e3ed147db8f8d3a179c5b24447139eba77522
Gustavo Delgado <gcdelgado@gmail.com>
2016-01-12 20:32:24 -0200

    criando teste groovy para insercao de processo pensandoodireito/sislegis-app#316

--- src/test/groovy/br/gov/mj/sislegis/app/endpoint/ProposicaoEndpointSpec.groovy
+++ src/test/groovy/br/gov/mj/sislegis/app/endpoint/ProposicaoEndpointSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 
 class ProposicaoEndpointSpec extends Specification {
 
-    def token = "Bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJkZTE5MjExOS0xYzJhLTQ4ZTAtYTI2Mi1mOTA5ZmFmMTQ0NTEiLCJleHAiOjE0NTAxMDg4NTAsIm5iZiI6MCwiaWF0IjoxNDUwMTA4NTUwLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L2F1dGgvcmVhbG1zL3Npc2xlZ2lzIiwiYXVkIjoic2lzbGVnaXMiLCJzdWIiOiJjNmZkMjhmMi0yMDU0LTRmMGEtOWU4My0yMjQzMzJlM2ZmNDAiLCJhenAiOiJzaXNsZWdpcyIsInNlc3Npb25fc3RhdGUiOiIzYmRiNjhjZi1iN2YzLTRlNzktYmVjZS1iNWVlMzE2YmNiZWEiLCJjbGllbnRfc2Vzc2lvbiI6IjA0ODcwNTVmLTlhNDEtNDRiOC04MGViLWU3MDMyNWIwMGJhYyIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vc2lzbGVnaXMubG9jYWwiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbInVzZXIiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50Iiwidmlldy1wcm9maWxlIl19fSwibmFtZSI6Ikd1c3Rhdm8gRGVsZ2FkbyIsInByZWZlcnJlZF91c2VybmFtZSI6Imd1c3Rhdm8iLCJnaXZlbl9uYW1lIjoiR3VzdGF2byIsImZhbWlseV9uYW1lIjoiRGVsZ2FkbyIsImVtYWlsIjoiZ2NkZWxnYWRvQGdtYWlsLmNvbSJ9.ZZqEbRqDqITOc7vFw8hVJHj0lg8SOTF2jw7_uPYoSKhfi40SjzJU7Sq1A1jX6QaQgocLa_RzLB9j2DrUP-buRTJalIzZzh354RdsqzAxbFB_LWFkbZc_jHA0EHkVWAPfe8PhRXyZtfVYrjseRsYxO6LIdd71gF34EjgRAvU26R9kKu8ufkbg_tylgQ3f9p8ttjz9HLlTW3dyEC36OC7Bt5p0KOUS0LEfPJDIJwR559xZArm7qni0cfTjW_ZJUn2tVIIzGv8PPm4_mk0X0-tjjipEkFZun5XvDrzOIlNGTUzyru5RCo19_jOXXWxuAIs4j0WbJ-_MoUhNpwjo2_PsUw"
+    def token = "Bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI5OTMxNzhkMi1lYmI4LTQ1ZjctOTY4ZC1mMDViYWExNjk2NGIiLCJleHAiOjE0NTI2MzgwNzUsIm5iZiI6MCwiaWF0IjoxNDUyNjM3Nzc1LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L2F1dGgvcmVhbG1zL3Npc2xlZ2lzIiwiYXVkIjoic2lzbGVnaXMiLCJzdWIiOiI1ZWU3Y2U3Ni1lMjEwLTRlYjYtOTY1NS00MzE5ZWIyNjg2NjQiLCJhenAiOiJzaXNsZWdpcyIsInNlc3Npb25fc3RhdGUiOiJiYTRmNGZlNi1mYWJhLTQyYTYtYTgxMS0wMmI3ODVhNDI1OGUiLCJjbGllbnRfc2Vzc2lvbiI6ImYxNzExY2E4LWY1MjktNGUzYy05OTk4LWYyOGRiZjJiYTUxOSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vc2lzbGVnaXMubG9jYWwiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbInVzZXIiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50Iiwidmlldy1wcm9maWxlIl19fSwibmFtZSI6Ikd1c3Rhdm8gRGVsZ2FkbyIsInByZWZlcnJlZF91c2VybmFtZSI6Imd1c3Rhdm8iLCJnaXZlbl9uYW1lIjoiR3VzdGF2byIsImZhbWlseV9uYW1lIjoiRGVsZ2FkbyIsImVtYWlsIjoiZ2NkZWxnYWRvQGdtYWlsLmNvbSJ9.SkAQoVRyW8gplEpI4l6xmndpwgk5Z95rULzdrl-V62kgAsiLfcGAckaTlF-U0wXbglKCZaSNydQ2I19DTrBmw_2YKDtCmeigujwB6wksDJcv-tkL30ps4kqOeq-flSBrwItRLn1cvjaHh6CRirZOqRfdt-3beKgNLarJ1k2dYJB6RJbhoSeBEjTQdofpmVhFoCGJMUkQbz0rZGkxIcfI-pTlo0u5ukf5wTdcsQxtRWN1HYd9Oqz7ITPWN2gTNQgmVjn9J0zEOw62CzRcYNPIJ0K1vsePxfdacaUI6BnSP_Q4nAb3rb2FeqoUrQ6tttQ-ypF9rX8kS8cEDeSh0GChkw"
     def client = new RESTClient("http://localhost:8080/")
     def cabecalho = [Authorization: token]
 
@@ -47,6 +47,18 @@ class ProposicaoEndpointSpec extends Specification {
         given:
         def caminho = "/sislegis/rest/proposicaos/setRoadmapComissoes"
         def dados = [idProposicao: 29, comissoes: ['PLEN', 'CAPADR', 'CCJC']]
+
+        when:
+        def resp = client.post(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
+
+        then:
+        assert resp.status == 200 // status 200 = Ok
+    }
+
+    def "deve inserir um processo do sei a partir do protocolo"(){
+        given:
+        def caminho = "/sislegis/rest/proposicaos/inserirProcessoSei"
+        def dados = [id: 7, nup: "123456"]
 
         when:
         def resp = client.post(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
