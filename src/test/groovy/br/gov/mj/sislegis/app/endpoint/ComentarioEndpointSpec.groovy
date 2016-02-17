e09debd564b07d442bfb3ce35d2a2eb573ef09fc
Gustavo Delgado <gcdelgado@gmail.com>
2016-02-17 15:32:19 -0200

    Criando testes para o endpoint de comentarios pensandoodireito/sislegis-app#126

--- src/test/groovy/br/gov/mj/sislegis/app/endpoint/ComentarioEndpointSpec.groovy
+++ src/test/groovy/br/gov/mj/sislegis/app/endpoint/ComentarioEndpointSpec.groovy
@@ -1,5 +1,6 @@
 package br.gov.mj.sislegis.app.endpoint
 
+import groovyx.net.http.ContentType
 import groovyx.net.http.RESTClient
 import spock.lang.Specification
 
@@ -7,6 +8,34 @@ class ComentarioEndpointSpec extends Specification{
 
     def restClient = new RESTClient("http://localhost/")
     def cabecalho = [Authorization: new BearerToken().obterToken()]
+
+    def "deve salvar um novo comentario"(){
+        given:
+        def caminho = "sislegis/rest/comentarios/"
+
+        def dados = [descricao  : "novo comentario teste teste",
+                     proposicao : [id: 162]]
+
+        when:
+        def response = restClient.post(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
+
+        then:
+        assert response.status == 201 // status 201 = Created
+    }
+
+    def "deve listar os comentarios de uma proposicao"(){
+        given:
+        def idProposicao = 162
+        def caminho = "sislegis/rest/comentarios/proposicao/" + idProposicao
+
+        when:
+        def response = restClient.get(path: caminho, headers: cabecalho)
+
+        then:
+        response.data.each {
+            println it
+        }
+    }
 
     def "deve ocultar um comentario"(){
         def idComentario = 166
