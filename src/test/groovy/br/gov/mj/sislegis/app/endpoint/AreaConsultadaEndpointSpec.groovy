d963a5c19359c36a22597e6f6d81a219a0efbaa1
Gustavo Delgado <gcdelgado@gmail.com>
2016-02-24 15:17:13 -0300

    refatoracao no teste de areas consultadas - endpoint, buscando ids dinamicamente pensandoodireito/sislegis-app#126

--- src/test/groovy/br/gov/mj/sislegis/app/endpoint/AreaConsultadaEndpointSpec.groovy
+++ src/test/groovy/br/gov/mj/sislegis/app/endpoint/AreaConsultadaEndpointSpec.groovy
@@ -4,11 +4,11 @@ import groovyx.net.http.ContentType
 import groovyx.net.http.RESTClient
 import spock.lang.Specification
 
-class AreaConsultadaEndpointSpec extends Specification{
+class AreaConsultadaEndpointSpec extends Specification {
     def restClient = new RESTClient("http://localhost/")
     def cabecalho = [Authorization: new BearerToken().obterToken()]
 
-    def "deve inserir uma nova area consultada"(){
+    def "deve inserir uma nova area consultada"() {
         given:
         def caminho = "/sislegis/rest/areaconsultadas/"
         def random = new Random()
@@ -22,9 +22,10 @@ class AreaConsultadaEndpointSpec extends Specification{
         assert resp.status == 201 // status 201 = Created
     }
 
-    def "deve consultar uma area consultada pelo id"(){
+    def "deve consultar uma area consultada pelo id"() {
         given:
-        def idAreaConsultada = 216
+        def areas = listarTodasAreas()
+        def idAreaConsultada = areas[0].id
         def caminho = "/sislegis/rest/areaconsultadas/" + idAreaConsultada
 
         when:
@@ -32,35 +33,41 @@ class AreaConsultadaEndpointSpec extends Specification{
 
         then:
         assert resp.status == 200 // status 200 = Ok
-        resp.data.each{
-            println it
-        }
+        println resp.data
     }
 
-    def "deve listar todas as areas consultadas"(){
-        given:
-        def caminho = "/sislegis/rest/areaconsultadas/"
-        def query = [start: 0, max: 300]
-
+    def "deve listar todas as areas consultadas"() {
         when:
-        def resp = restClient.get(path: caminho, query: query, headers: cabecalho)
+        def areas = listarTodasAreas()
 
         then:
-        resp.data.each{
+        areas.each {
             println it
         }
     }
 
-    def "deve alterar uma area consultada"(){
+    def "deve alterar uma area consultada"() {
         given:
-        def idAreaConsultada = 216
+        def areas = listarTodasAreas()
+        def idAreaConsultada = areas[0].id
+        def random = new Random()
+        def nomeAleatorio = random.nextInt()
         def caminho = "/sislegis/rest/areaconsultadas/" + idAreaConsultada
-        def dados = [id: idAreaConsultada, descricao: "Area Consultada Teste ALTERADO"]
+        def dados = [id: idAreaConsultada, descricao: "Area Consultada Teste ALTERADA!" + nomeAleatorio]
 
         when:
         def resp = restClient.put(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
 
         then:
         assert resp.status == 204 // status 204 = No Content
+    }
+
+    def listarTodasAreas() {
+        def caminho = "/sislegis/rest/areaconsultadas/"
+        def query = [start: 0, max: 300]
+
+        def resp = restClient.get(path: caminho, query: query, headers: cabecalho)
+
+        return resp.data
     }
 }
