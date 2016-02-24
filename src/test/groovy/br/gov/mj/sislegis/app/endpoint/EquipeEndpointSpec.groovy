7c5730563324e8c291ea68df88425a8c1112792c
Gustavo Delgado <gcdelgado@gmail.com>
2016-02-23 22:14:11 -0300

    refatoracao no teste de equipes - endpoint, inserindo uma nova, realizando os demais testes e excluindo a equipe inserida pensandoodireito/sislegis-app#126

--- src/test/groovy/br/gov/mj/sislegis/app/endpoint/EquipeEndpointSpec.groovy
+++ src/test/groovy/br/gov/mj/sislegis/app/endpoint/EquipeEndpointSpec.groovy
@@ -11,8 +11,7 @@ class EquipeEndpointSpec extends Specification {
     def "deve inserir uma nova equipe"() {
         given:
         def caminho = "/sislegis/rest/equipes"
-        def dados = [nome              : "Nova equipe",
-                     listaEquipeUsuario: []]
+        def dados = [nome: "Nova equipe", listaEquipeUsuario: []]
 
         when:
         def resp = restClient.post(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
@@ -21,9 +20,51 @@ class EquipeEndpointSpec extends Specification {
         assert resp.status == 201 // status 201 = Created
     }
 
+    def "deve consultar uma equipe pelo id"() {
+        given:
+        def equipes = listarTodasEquipes()
+        def ultimaEquipe = equipes[equipes.size - 1]
+        def idEquipe = ultimaEquipe.id
+        def caminho = "/sislegis/rest/equipes/" + idEquipe
+
+        when:
+        def resp = restClient.get(path: caminho, headers: cabecalho)
+
+        then:
+        assert resp.status == 200 // status 200 = Ok
+        println resp.data
+    }
+
+    def "deve listar todas as equipes"() {
+        when:
+        def equipes = listarTodasEquipes()
+
+        then:
+        equipes.each {
+            println it
+        }
+    }
+
+    def "deve alterar uma equipe"() {
+        given:
+        def equipes = listarTodasEquipes()
+        def ultimaEquipe = equipes[equipes.size - 1]
+        def idEquipe = ultimaEquipe.id
+        def caminho = "/sislegis/rest/equipes/" + idEquipe
+        def dados = [id: idEquipe, nome: "Equipe ALTERADA", listaEquipeUsuario: []]
+
+        when:
+        def resp = restClient.put(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
+
+        then:
+        assert resp.status == 204 // status 204 = No Content
+    }
+
     def "deve excluir uma equipe"() {
         given:
-        def idEquipe = 226
+        def equipes = listarTodasEquipes()
+        def ultimaEquipe = equipes[equipes.size - 1]
+        def idEquipe = ultimaEquipe.id
         def caminho = "/sislegis/rest/equipes/" + idEquipe
 
         when:
@@ -33,45 +74,12 @@ class EquipeEndpointSpec extends Specification {
         assert resp.status == 204 // status 204 = No Content
     }
 
-    def "deve consultar uma equipe pelo id"() {
-        given:
-        def idEquipe = 224
-        def caminho = "/sislegis/rest/equipes/" + idEquipe
-
-        when:
-        def resp = restClient.get(path: caminho, headers: cabecalho)
-
-        then:
-        assert resp.status == 200 // status 200 = Ok
-        resp.data.each {
-            println it
-        }
-    }
-
-    def "deve listar todas as equipes"() {
-        given:
+    def listarTodasEquipes() {
         def caminho = "/sislegis/rest/equipes"
         def query = [start: 0, max: 300]
 
-        when:
         def resp = restClient.get(path: caminho, query: query, headers: cabecalho)
 
-        then:
-        resp.data.each {
-            println it
-        }
-    }
-
-    def "deve alterar uma equipe"() {
-        given:
-        def idEquipe = 224
-        def caminho = "/sislegis/rest/equipes/" + idEquipe
-        def dados = [id: idEquipe, nome: "Equipe ALTERADA", listaEquipeUsuario: []]
-
-        when:
-        def resp = restClient.put(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
-
-        then:
-        assert resp.status == 204 // status 204 = No Content
+        return resp.data
     }
 }
