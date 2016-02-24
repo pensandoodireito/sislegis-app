b18ae4064e34ea960e2a0cd1301b6be73b55a353
Gustavo Delgado <gcdelgado@gmail.com>
2016-02-23 22:07:50 -0300

    refatoracao no teste de tipos de encaminhamentos - endpoint, inserindo um novo, realizando os demais testes e excluindo o tipo inserido pensandoodireito/sislegis-app#126

--- src/test/groovy/br/gov/mj/sislegis/app/endpoint/TipoEncaminhamentoEndpointSpec.groovy
+++ src/test/groovy/br/gov/mj/sislegis/app/endpoint/TipoEncaminhamentoEndpointSpec.groovy
@@ -20,9 +20,51 @@ class TipoEncaminhamentoEndpointSpec extends Specification{
         assert resp.status == 201 // status 201 = Created
     }
 
+    def "deve consultar um tipo de encaminhamento pelo id"(){
+        given:
+        def tipos = listarTodosTipos()
+        def ultimoTipo = tipos[tipos.size - 1]
+        def idTipoEncaminhamento = ultimoTipo.id
+        def caminho = "/sislegis/rest/tiposencaminhamentos/" + idTipoEncaminhamento
+
+        when:
+        def resp = restClient.get(path: caminho, headers: cabecalho)
+
+        then:
+        assert resp.status == 200 // status 200 = Ok
+        println resp.data
+    }
+
+    def "deve listar todos os tipos de encaminhamentos"(){
+        when:
+        def tipos = listarTodosTipos()
+
+        then:
+        tipos.each{
+            println it
+        }
+    }
+
+    def "deve alterar um tipo de encaminhamento"(){
+        given:
+        def tipos = listarTodosTipos()
+        def ultimoTipo = tipos[tipos.size - 1]
+        def idTipoEncaminhamento = ultimoTipo.id
+        def caminho = "/sislegis/rest/tiposencaminhamentos/" + idTipoEncaminhamento
+        def dados = [id: idTipoEncaminhamento, nome: "Tipo Encaminhamento ALTERADO"]
+
+        when:
+        def resp = restClient.put(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
+
+        then:
+        assert resp.status == 204 // status 204 = No Content
+    }
+
     def "deve excluir um tipo de encaminhamento"(){
         given:
-        def idTipoEncaminhamento = 219
+        def tipos = listarTodosTipos()
+        def ultimoTipo = tipos[tipos.size - 1]
+        def idTipoEncaminhamento = ultimoTipo.id
         def caminho = "/sislegis/rest/tiposencaminhamentos/" + idTipoEncaminhamento
 
         when:
@@ -32,45 +74,12 @@ class TipoEncaminhamentoEndpointSpec extends Specification{
         assert resp.status == 204 // status 204 = No Content
     }
 
-    def "deve consultar um tipo de encaminhamento pelo id"(){
-        given:
-        def idTipoEncaminhamento = 180
-        def caminho = "/sislegis/rest/tiposencaminhamentos/" + idTipoEncaminhamento
-
-        when:
-        def resp = restClient.get(path: caminho, headers: cabecalho)
-
-        then:
-        assert resp.status == 200 // status 200 = Ok
-        resp.data.each{
-            println it
-        }
-    }
-
-    def "deve listar todos os tipos de encaminhamentos"(){
-        given:
+    def listarTodosTipos(){
         def caminho = "/sislegis/rest/tiposencaminhamentos"
         def query = [start: 0, max: 300]
 
-        when:
         def resp = restClient.get(path: caminho, query: query, headers: cabecalho)
 
-        then:
-        resp.data.each{
-            println it
-        }
-    }
-
-    def "deve alterar um tipo de encaminhamento"(){
-        given:
-        def idTipoEncaminhamento = 180
-        def caminho = "/sislegis/rest/tiposencaminhamentos/" + idTipoEncaminhamento
-        def dados = [id: idTipoEncaminhamento, nome: "Tipo Encaminhamento ALTERADO"]
-
-        when:
-        def resp = restClient.put(path: caminho, body: dados, headers: cabecalho, requestContentType: ContentType.JSON)
-
-        then:
-        assert resp.status == 204 // status 204 = No Content
+        return resp.data
     }
 }
